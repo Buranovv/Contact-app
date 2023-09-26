3e83ff38281b2b718c565877e6cfafe79ffca19b
Buranovv <zahiriddinburanov007@gmail.com>
2023-09-26 22:51:33 +0500

    done

---
+++ src/commponents/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer className="py-3 text-center">Made by Zahiriddin Buranov</footer>
+    );
+  }
+}
+
+export default Footer;
